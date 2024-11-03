74e0f7e8e2904eb842e3bfb0f498323c568b7030
Gemy <gemy46349@gmail.com>
2024-11-03 18:23:23 +0200

    Change some styles

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
-      allowedOrigins: ["nextjs-fullstack-todo", "*.nextjs-fullstack-todo"],
+      allowedOrigins: [],
     },
   },
 };
